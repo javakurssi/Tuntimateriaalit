05dbbc5b322fd492790f3979b9049ed938b38d6a
koodiohto <ohto.rainio@fortum.com>
2023-10-23 19:58:53 +0300

    Poistettu turhia tiedostoja ja lisätty tunti1 perusmateriaali

---
+++ src/main/java/tunti1/Tunti1.java
@@ -0,0 +1,106 @@
+package tunti1;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tunti1 {
+
+    public static void main(String[] args) {
+        // Esimerkkejä muuttujista
+        int number = 42;
+        double pi = 3.14;
+        String message = "Hello, world!";
+        boolean isJavaFun = true;
+
+        // Esimerkkejä kontrollirakenteista
+        if (isJavaFun) {
+            System.out.println("Java is fun!");
+        } else {
+            System.out.println("Java is not fun?");
+        }
+
+        int dayOfWeek = 3;
+        switch (dayOfWeek) {
+            case 1:
+                System.out.println("It's Monday");
+                break;
+            case 2:
+                System.out.println("It's Tuesday");
+                break;
+            // ...
+
+            default:
+                System.out.println("It's some other day");
+        }
+
+        int x = 5;
+        int y = (x > 0) ? 1 : -1;
+
+        // Esimerkkejä silmukoista
+        int[] numbersArray = { 1, 2, 3, 4, 5 };
+        for (int num : numbersArray) {
+            System.out.print(num + " ");
+        }
+
+        int i = 0;
+        while (i < 5) {
+            System.out.print(i + " ");
+            i++;
+        }
+
+        // Esimerkkejä metodeista ja parametreistä
+        int sum = add(3, 5);
+        System.out.println("Sum: " + sum);
+
+        // Esimerkkejä taulukoista ja listoista
+        int[] array = { 1, 2, 3, 4, 5 };
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        // Esimerkkejä olio-ohjelmoinnin perusteista
+        Person person = new Person("John Doe", 25);
+        System.out.println("Person: " + person.getName() + ", Age: " + person.getAge());
+
+        // Esimerkkejä poikkeuskäsittelystä
+        try {
+            int result = divide(10, 0);
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+    // Esimerkkejä metodeista
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+    // Esimerkkejä olio-ohjelmoinnin perusteista
+    static class Animal {
+        private String name;
+        private int weight;
+        private int age;
+
+        public Animal(String name, int weight) {
+            this.name = name;
+            this.weight = weight;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+    }
+}
