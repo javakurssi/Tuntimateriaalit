ea488854d39d7c8ddaebf7dd0be24fb27692954d
koodiohto <ohto.rainio@fortum.com>
2024-11-13 19:20:44 +0200

    suomennettu

--- src/main/java/tunti4/MuotoEsimerkki.java
+++ src/main/java/tunti4/MuotoEsimerkki.java
@@ -23,17 +23,17 @@ public class MuotoEsimerkki {
 			System.out.println("Ympyrä on todella ympyrä");
 		}
 		
-		// Verrataan muotoja
+		// Verrataan muotoja, myös olioiden castaus
 		AbstraktiMuoto aYmpyra = (AbstraktiMuoto) ympyra;
 		AbstraktiMuoto aSuorakulmio = (AbstraktiMuoto) suorakulmio;
 		
 		int comparisonResult = aYmpyra.compareTo(aSuorakulmio);
 		if (comparisonResult < 0) {
-			System.out.println("Ympyrä has a smaller area than Suorakulmio.");
+			System.out.println("Ympyrä on pienempi kuin Suorakulmio.");
 		} else if (comparisonResult > 0) {
-			System.out.println("Ympyrä has a larger area than Suorakulmio.");
+			System.out.println("Ympyrä on suurempi kuin Suorakulmio.");
 		} else {
-			System.out.println("Ympyrä and Suorakulmio have the same area.");
+			System.out.println("Ympyrä ja Suorakulmio ovat saman kokoisia.");
 		}
 		
 	    List<AbstraktiMuoto> muodot = new ArrayList<>();
@@ -67,7 +67,6 @@ abstract class AbstraktiMuoto implements Muoto, Comparable<AbstraktiMuoto> {
 		laskuri++;
 	}
 	
-	@Override
 	public double laskePintaAla() {
 		kasvataLaskuria();
 		return Math.PI;
